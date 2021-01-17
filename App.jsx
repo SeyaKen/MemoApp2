dfe5bcc51dd6cc5739b3f804a30b13a7caef2cfb
Seya <123seyaken@gmail.com>
2021-01-17 09:08:41 +0900

    button create

--- App.jsx
+++ App.jsx
@@ -1,27 +1,112 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import Hello from './src/components/Hello';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Hello bang>Die</Hello>
-      <Hello style={{ fontSize: 16 }}>Small World</Hello>
-      {/* {{}}の中にcssのスタイルを書く */}
-      {/* ここではbang={true}だがbangだけでbang={true｝の意味になる */}
-      {/* eslint-disable */}
-      <StatusBar style="auto" />
+
+      <View style={styles.appbar}>
+        <View style={styles.appbarInner}>
+          <Text style={styles.appbarTitle}>Memo Appp</Text>
+          <Text style={styles.appbarRight}>ログアウト</Text>
+        </View>
+      </View>
+
+      <View>
+        <View style={styles.memoListItem}>
+          <View>
+            <Text style={styles.memoListItemTitle}>買い物リスト</Text>
+            <Text style={styles.memoListItemDate}>2020/12/24 10:00</Text>
+            {/* 左側 */}
+          </View>
+          <View>
+            <Text>X</Text>
+            {/* 削除ボタン */}
+          </View>
+        </View>
+      </View>
+
+      <View style={styles.circleButton}>
+        <Text style={styles.circleButtonLabel}>+</Text>
+      </View>
     </View>
+  //  <Hello style={{ fontSize: 16 }}>Small World</Hello>
+  //  {{}}の中にcssのスタイルを書く */}
+  //  ここではbang={true}だがbangだけでbang={true｝の意味になる */}
+  //  eslint-disable
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#f0f4f8',
+  },
+  appbar: {
+    width: '100%',
+    height: 104,
+    backgroundColor: '#467fd3',
+    justifyContent: 'flex-end',
+  },
+  appbarInner: {
     alignItems: 'center',
+  },
+  appbarRight: {
+    position: 'absolute',
+    right: 19,
+    bottom: 16,
+    color: 'rgba(255, 255, 255, 0.8)',
+  },
+  appbarTitle: {
+    marginBottom: 8,
+    fontSize: 22,
+    lineHeight: 32,
+    color: '#fff',
+    fontWeight: 'bold',
+  },
+  memoListItem: {
+    backgroundColor: '#ffffff',
+    flexDirection: 'row',
+    // フレックスボックスの向きがreactnativeだと普通と逆だから、flexDirection: 'row'と書くと普通になる
+    justifyContent: 'space-between',
+    paddingVertical: 16,
+    // 垂直方向のpadding,verticalは「垂直の」という意味
+    paddingHorizontal: 19,
+    // 横方向のpadding,horizontalは「水平の」という意味
+    alignItems: 'center',
+    borderBottomWidth: 1,
+    borderColor: 'rgba(0, 0, 0, 0.15)',
+  },
+  memoListItemTitle: {
+    fontSize: 16,
+    lineHeight: 32,
+  },
+  memoListItemDate: {
+    fontSize: 12,
+    lineHeight: 16,
+    color: '#848484',
+  },
+  circleButton: {
+    backgroundColor: '#467fd3',
+    width: 64,
+    height: 64,
+    borderRadius: 32,
     justifyContent: 'center',
+    alignItems: 'center',
+    position: 'absolute',
+    right: 40,
+    bottom: 40,
+    shadow: '#000',
+    shadowOffset: { width: 0, height: 8 },
+    shadowOpacity: 0.25,
+    shadowRadius: 8,
+    // shadowRadiusプロパティはiosにしか対応していない
+    elevation: 8,
+    // elevationはandroidにしか対応していない
+  },
+  circleButtonLabel: {
+    color: '#fff',
+    fontSize: 40,
+    lineHeight: 40,
   },
 });
